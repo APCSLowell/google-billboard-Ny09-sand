8b85eeacb305df91b3c2bb057f791310edb5fa97
Ny09-sand <nicholas.yangwt@gmail.com>
2025-01-26 15:05:34 -0800

    Update GoogleBillboard.java

--- src/main/java/GoogleBillboard.java
+++ src/main/java/GoogleBillboard.java
@@ -2,7 +2,7 @@
 
 public class GoogleBillboard {
 	public final static String e = "2.7182818284590452353602874713526624977572470936999595749669676277240766303535475945713821785251664274274663919320030599218174135966290435";  
-	
+	//uhh
 	public static void main(final String[] args) {
 		double dNum; 
 		for ( int i = 1 ;  i < e.length() - 11 ; i ++ ) { 
